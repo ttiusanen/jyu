8e22e781b091a647facde15f27eb1e6873c4b806
Timo Tiusanen <ttiusanen@gmail.com>
2020-01-11 12:10:31 +0200

    Initial commit

---
+++ src/test/java/fi/jyu/issuetracker/IssuetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.jyu.issuetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssuetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
