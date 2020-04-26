64a812caace46aa2b9fb8703fa2b1725e99870f1
Timo Tiusanen <ttiusanen@gmail.com>
2020-04-26 15:02:55 +0300

    Added method for quering saved issues

--- src/main/java/fi/jyu/issuetracker/controllers/FormSubmissionController.java
+++ src/main/java/fi/jyu/issuetracker/controllers/FormSubmissionController.java
@@ -1,10 +1,13 @@
 package fi.jyu.issuetracker.controllers;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import fi.jyu.issuetracker.dao.models.Issue;
 import fi.jyu.issuetracker.dao.repositories.IssueRepository;
@@ -16,14 +19,20 @@ public class FormSubmissionController {
 	private IssueRepository issueRepository;
 	
 	@GetMapping("/newIssue")
-	public String greetingForm(Model model) {
+	public String issueForm(Model model) {
 	    model.addAttribute("issue", new Issue());
 	    return "index";
-	  }
+	}
 	
 	@PostMapping("/newIssue")
-	public String greetingSubmit(@ModelAttribute Issue issue) {
+	public String issueSubmit(@ModelAttribute Issue issue) {
 		issueRepository.save(issue);
 	    return "issues";
-	  }
+	}
+	
+	@GetMapping("/issues")
+	@ResponseBody
+	public List<Issue> issues() {
+	    return issueRepository.findAll();
+	}
 }
